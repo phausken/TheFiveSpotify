02e53a630c7ae951364b0b5c5b60a164cfbfe8ec
Peter Hausken <phausken@gmail.com>
2017-08-22 11:09:26 -0400

    created and tested session API util functions

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, logout, signUp} from './util/session_api_util.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to The Five Spotify</h1>, root);
+
+  window.login = login;
+  window.logout = logout;
+  window.signUp = signUp;
 });
