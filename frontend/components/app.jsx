3263e21490ed252b72a295b631fda9ec1e599ff6
Peter Hausken <phausken@gmail.com>
2017-08-22 11:58:21 -0400

    Finish app and root component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>The Five Spotify</h1>
+  </div>
+);
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { login, logout, signup } from './actions/session_actions';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
 
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>Welcome to The Five Spotify</h1>, root);
 
