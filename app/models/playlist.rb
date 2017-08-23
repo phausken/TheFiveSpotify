af37c303b212aad7758caaab31f174e679896989
Peter Hausken <phausken@gmail.com>
2017-08-23 16:40:52 -0400

    added rails associations for playlists, songs, playlist_adds

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,13 @@
 class Playlist < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :playlist_adds,
+    through: :PlaylistAdd,
+    primary_key: :id,
+    foreign_key: :playlist_id
+
+  has_many :songs
+    through: :playlist_adds,
+    source: :Songs
 
 end
--- app/models/playlist_add.rb
+++ app/models/playlist_add.rb
@@ -1,2 +1,6 @@
 class PlaylistAdd < ActiveRecord::Base
+  validates :playlist_id, :song_id, presence: true
+
+  belongs_to :playlist
+  belongs_to :song
 end
