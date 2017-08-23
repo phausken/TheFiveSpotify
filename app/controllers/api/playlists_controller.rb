c41b2d8678783f0fc612c35e508ac6e0e4a21780
Peter Hausken <phausken@gmail.com>
2017-08-23 17:32:27 -0400

    Add routes and methods for Playlist controller

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,47 @@
+class Api::PlaylistsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+    if @playlist.save
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @playlist = Playlist.find_by(playlist_params)
+    render :show
+  end
+
+  def edit
+    @playlist = Playlist.find_by(playlist_params)
+    if @playlist.update
+      render :show
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @playlist = Playlist.all
+  end
+
+  def destroy
+    @playlist = Playlist.find_by(playlist_params)
+    if @playlist
+      @playlist.delete
+      render :new
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
+  end
+
+private
+def playlist_params
+  params.require(:playlist).permit(:name)
+end
+end
