158b8dfc98918d3fc9006dc2a70d28e1b80ede62
Peter Hausken <phausken@gmail.com>
2017-08-23 16:16:20 -0400

    Add Song model

--- db/migrate/20170823201308_create_songs.rb
+++ db/migrate/20170823201308_create_songs.rb
@@ -1,8 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-
+      t.string :title, null: false
       t.timestamps null: false
     end
+
+    add_index :songs, :title
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823200946) do
+ActiveRecord::Schema.define(version: 20170823201308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170823200946) do
   end
 
   add_index "playlists", ["name"], name: "index_playlists_on_name", using: :btree
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "songs", ["title"], name: "index_songs_on_title", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
