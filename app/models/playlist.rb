ba56ad74b99371641a9eb697a3cdb82e474e15c8
Peter Hausken <phausken@gmail.com>
2017-08-24 09:16:53 -0400

    Add playlist_ids to user state

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,8 @@
 class Playlist < ActiveRecord::Base
   validates :name, presence: true
 
+  belongs_to :user
+
   has_many :playlist_adds
 
   has_many :songs,
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,4 @@
 json.extract! user, :id, :username
+json.playlist_ids do
+  json.array! (user.playlists.ids)
+end
